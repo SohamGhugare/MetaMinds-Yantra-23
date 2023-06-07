8f910a0be12ca81ba0e9f5ed4605b3d85d389865
chirantan2003 <chirantan2003@gmail.com>
2023-06-08 02:39:41 +0530

    feat: home page

---
+++ venuezy/src/pages/Hall.js
@@ -0,0 +1,21 @@
+import "./Hall.css";
+import Navbar from "../components/navbar/Navbar";
+import React from "react";
+
+const Hall = () => {
+  return (
+    <div>
+      <Navbar />
+      {/* <div className="venues">
+        <VenueBox name="Hall" />
+        <VenueBox name="Lawns" />
+        <VenueBox name="Garages" />
+        <VenueBox name="Labs" />
+        <VenueBox name="Auditorium" />
+      </div>
+      <Venues /> */}
+    </div>
+  );
+};
+
+export default Hall;
