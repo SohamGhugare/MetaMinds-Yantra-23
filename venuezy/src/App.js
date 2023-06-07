fa7a07c31040e19341f55b039467c275ac9c8e39
MehakK1311 <mehakhanna1311@gmail.com>
2023-06-07 22:14:55 +0530

    feat:auth pages

--- venuezy/src/App.js
+++ venuezy/src/App.js
@@ -1,14 +1,22 @@
-import "./App.css";
-import Navbar from "./components/navbar/Navbar.js";
+import {Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import SigninPage from "./pages/patient/SigninPage";
+import SignupPage from "./pages/patient/SignupPage";
+import SigninPageDoc from "./pages/doctor/SigninPage";
+import SignupPageDoc from "./pages/doctor/SignupPage";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <Navbar />
-        <p>Venuezy</p>
-      </header>
-    </div>
+   
+    <Routes>
+      <Route path="/" element={<HomePage />}/>
+      <Route path="/patient/login" element={<SigninPage/>}/>
+      <Route path="/patient/register" element={<SignupPage/>}/>
+      <Route path="/doctor/login" element={<SigninPageDoc/>}/>
+      <Route path="/doctor/register" element={<SignupPageDoc/>}/>
+    </Routes>
+  
   );
 }
 
